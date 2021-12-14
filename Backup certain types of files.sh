8acdb68333485753448a9e529b8548111419a44b
Deanna121 <92946845+Deanna121@users.noreply.github.com>
2021-12-13 18:47:57 -0800

    Update Backup certain types of files.sh

--- Backup certain types of files.sh
+++ Backup certain types of files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-  
+
 
 if [ -z $3 ]
 then
@@ -18,20 +18,15 @@ echo "file extention is: $fileExtention"
 echo "target dir is: $targetDir"
 echo "backup directory is: $destinationDir" 
 
-# figure out how the user can tell me if they want quiet mode or normal mode
 
-# test and see if the user has given me a q
-# test to make sure the target directory exists
-         
+
 if [ ! -d "$targetDir" ]
-then     
-        echo "$targetDir does not exist, please enter a valid target dir"
-        exit 0;
-fi           
-      
-# figure out find command for removing the file 
 
-# find [targetdir] [name|type|date|lastmodified] -exec [command you want to execute on the found files] {} \; 
-    
-find "$targetDir"  -name *"$fileExtention" -exec cp {} $destinationDir \;
-    
+then
+        echo "$targetDir" ]
+        exit 0;
+fi
+
+
+
+find $targetDir  -name *$fileExtention -exec cp {} $destinationDir \;
