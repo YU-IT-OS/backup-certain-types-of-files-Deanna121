17266665dc7c0f88da8b9eaffe4029a2f4ab6c36
Deanna121 <92946845+Deanna121@users.noreply.github.com>
2021-12-13 18:39:02 -0800

    Create Final.sh

---
+++ Final.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+if [ -z $3 ]
+then
+        echo "Usage: script [file extension] [target directory] [backup directory] 
+        Example: script .text /home/arnold /backup 
+        this is backs up files from a certain directory to anohter 
+        "
+exit 0;
+fi
+
+fileExtention="$1"
+targetDir="$2"
+destinationDir="$3"
+
+echo "file extention is: $fileExtention"
+echo "target dir is: $targetDir"
+echo "backup directory is: $destinationDir" 
+
+
+
+if [ ! -d "$targetDir" ]
+then
+        echo "$targetDir does not exist, please enter a valid target dir"
+        exit 0;
+fi
+
+
+
+find "$targetDir"  -name *"$fileExtention" -exec cp {} $destinationDir \;
