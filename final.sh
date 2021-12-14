b551981fc557a7a92caec9ea5521425331c2d9ea
Deanna121 <92946845+Deanna121@users.noreply.github.com>
2021-12-13 18:58:08 -0800

    Update final.sh

--- final.sh
+++ final.sh
@@ -14,9 +14,9 @@ fileExtention="$1"
 targetDir="$2"
 destinationDir="$3"
 
-echo "fileExtention is: $fileExtention"
-echo "targetDir is: $targetDir"
-echo "backupDirectory is: $destinationDir" 
+echo "file extention is: "$fileExtention"
+echo "target dir is: "$targetDir"
+echo "backup directory is: "$destinationDir" 
 
 
 
